8f0c2e776b3f73edcde80b0951e6aede8376dc83
Lasse Eskildsen <leskil99@gmail.com>
2020-06-15 14:50:45 +0200

    Added unit test

---
+++ src/TimeApi/TimeApi.Tests/SampleUnitTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TimeApi.Tests
+{
+    public class SampleUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true, "The value is true");
+        }
+    }
+}
